20a288e71ede490f2dd537053d128018d5ce2223
Byron Ellacott <code@bje.id.au>
2016-09-27 13:36:42 +1000

    Fix up loaders, and add a snapshot-creating endpoint

--- src/main/java/net/apnic/whowas/App.java
+++ src/main/java/net/apnic/whowas/App.java
@@ -14,16 +14,19 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.actuate.endpoint.Endpoint;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
-import org.springframework.core.io.ResourceLoader;
+import org.springframework.core.annotation.Order;
 import org.springframework.jdbc.core.JdbcOperations;
 
 import javax.annotation.PostConstruct;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.util.List;
 import java.util.zip.DeflaterOutputStream;
 import java.util.zip.InflaterInputStream;
 
@@ -43,52 +46,84 @@ public class App {
     }
 
     @Bean
-    @ConditionalOnProperty(value="from.database")
+    @Order(value=2)
+    @ConditionalOnProperty(value="spring.datasource.url")
     public Loader ripeLoader(JdbcOperations operations) {
         return new RipeDbLoader(operations);
     }
 
     @Bean
-    @ConditionalOnProperty(value="from.snapshot")
+    @Order(value=1)
+    @ConditionalOnProperty(value="snapshot.file")
     @SuppressWarnings("unchecked")
-    public Loader fileLoader(@Value("${snapshot.file}") String snapshotFile) {
+    public Loader fileLoader(@Value("${snapshot.file}") String snapshotFile, ApplicationContext context) {
         return () -> {
             LOGGER.info("Restoring snapshot from file {}", snapshotFile);
-            try (InputStream resourceStream = resourceLoader.getResource(snapshotFile).getInputStream();
+            try (InputStream resourceStream = context.getResource("file:///" + snapshotFile).getInputStream();
                  InflaterInputStream zipStream = new InflaterInputStream(resourceStream);
                  FSTObjectInput objStream = new FSTObjectInput(zipStream)) {
                  return (AvlTree<IP, History, IpInterval>)objStream.readObject();
-            } catch (IOException|ClassNotFoundException ex) {
-                LOGGER.error("Invalid snapshot file");
-                throw ex;
             }
         };
     }
 
-    @Autowired
-    private ResourceLoader resourceLoader;
-
-    @Autowired
-    private Loader loader;
+    @Autowired(required=false)
+    private List<Loader> loaders;
 
     @PostConstruct
     public void buildTree() {
         LOGGER.info("Loading history from configured sources");
-        try {
-            tree = loader.loadTree();
-        } catch (Exception ex) {
-            LOGGER.error("Failed to load data with {}", loader, ex);
-            throw new RuntimeException(ex);
+
+        // Try loaders in order until one succeeds
+        for (Loader loader : loaders) {
+            LOGGER.info("Invoking loader: {}", loader);
+            try {
+                tree = loader.loadTree();
+                break;
+            } catch (Exception ex) {
+                LOGGER.error("Failed to load data with {}: {}", loader, ex.getLocalizedMessage());
+            }
         }
+        LOGGER.info("Tree construction completed, {} entries", tree.size());
     }
 
-    private void writeSnapshot(String target) {
+    @Bean
+    @ConditionalOnProperty(value="snapshot.file")
+    Endpoint<Boolean> snapshotEndpoint(@Value("${snapshot.file}") String snapshotFile) {
+        return new Endpoint<Boolean>() {
+            @Override
+            public String getId() {
+                return "snapshot";
+            }
+
+            @Override
+            public boolean isEnabled() {
+                return true;
+            }
+
+            @Override
+            public boolean isSensitive() {
+                return true;
+            }
+
+            @Override
+            public Boolean invoke() {
+                try {
+                    writeSnapshot(snapshotFile);
+                } catch (IOException ioex) {
+                    LOGGER.error("Could not write snapshot file", ioex);
+                    return false;
+                }
+                return true;
+            }
+        };
+    }
+
+    private void writeSnapshot(String target) throws IOException {
         try (FileOutputStream fileOutput = new FileOutputStream(target);
              DeflaterOutputStream zipOutput = new DeflaterOutputStream(fileOutput);
              FSTObjectOutput objOutput = new FSTObjectOutput(zipOutput)) {
             objOutput.writeObject(tree);
-        } catch (IOException ioex) {
-            LOGGER.error("Could not write snapshot file", ioex);
         }
     }
 }
