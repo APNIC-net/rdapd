b5eb0732994ec0237c14b0c30d247dba6c8c97ac
Thomas Miller <tmiller@apnic.net>
2017-10-23 13:22:05 +1000

    Removed commented code

--- src/main/java/net/apnic/whowas/App.java
+++ src/main/java/net/apnic/whowas/App.java
@@ -1,14 +1,8 @@
 package net.apnic.whowas;
 
 import net.apnic.whowas.history.History;
-//import net.apnic.whowas.history.ObjectHistory;
-//import net.apnic.whowas.history.ObjectIndex;
-//import net.apnic.whowas.intervaltree.IntervalTree;
 import net.apnic.whowas.loaders.RipeDbLoader;
 import net.apnic.whowas.progress.Bar;
-//import net.apnic.whowas.types.IP;
-//import net.apnic.whowas.types.IpInterval;
-//import net.apnic.whowas.types.Tuple;
 import org.nustaq.serialization.FSTObjectInput;
 import org.nustaq.serialization.FSTObjectOutput;
 import org.slf4j.Logger;
@@ -37,13 +31,11 @@ import java.io.InputStream;
 import java.time.ZoneId;
 import java.time.ZonedDateTime;
 import java.time.temporal.ChronoUnit;
-//import java.util.Optional;
 import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.Future;
 import java.util.Properties;
-//import java.util.stream.Stream;
 import java.util.zip.DeflaterOutputStream;
 import java.util.zip.InflaterInputStream;
 
@@ -69,48 +61,6 @@ public class App {
         app.setDefaultProperties(defaultProps);
         app.run(args);
     }
-
-    /*@Bean
-    public IntervalTree<IP, ObjectHistory, IpInterval> ipListIntervalTree() {
-        return new IntervalTree<IP, ObjectHistory, IpInterval>() {
-            @Override
-            public Stream<Tuple<IpInterval, ObjectHistory>>
-                equalToAndLeastSpecific(IpInterval range) {
-                return history.getTree().equalToAndLeastSpecific(range)
-                        .flatMap(p -> history
-                                .historyForObject(p.snd())
-                                .map(Stream::of)
-                                .orElse(Stream.empty())
-                                .map(h -> new Tuple<>(p.fst(), h)));
-            }
-
-            @Override
-            public Optional<ObjectHistory> exact(IpInterval range) {
-                return history.getTree().exact(range)
-                        .flatMap(history::historyForObject);
-            }
-
-            @Override
-            public Stream<Tuple<IpInterval, ObjectHistory>> intersecting(IpInterval range) {
-                return history.getTree().intersecting(range)
-                        .flatMap(p -> history
-                                .historyForObject(p.snd())
-                                .map(Stream::of)
-                                .orElse(Stream.empty())
-                                .map(h -> new Tuple<>(p.fst(), h)));
-            }
-
-            @Override
-            public int size() {
-                return history.getTree().size();
-            }
-        };
-    }*/
-
- /*   @Bean
-    public ObjectIndex objectIndex() {
-        return history;
-    }*/
 
     @Bean
     public TaskExecutor taskScheduler() {
