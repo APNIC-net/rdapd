31acb64987de077ea83f08650dd6df98f89ae9c3
Jim Vella <jim@apnic.net>
2017-11-20 11:58:33 +1000

    Removed unused import

--- src/main/java/net/apnic/whowas/loaders/config/LoaderConfiguration.java
+++ src/main/java/net/apnic/whowas/loaders/config/LoaderConfiguration.java
@@ -30,7 +30,6 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.ApplicationContext;
-import org.springframework.jdbc.core.JdbcOperations;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.scheduling.annotation.Scheduled;
 
