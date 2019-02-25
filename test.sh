586f6506df37223750c3e1bf52c9ebb77d85013c
Alex Avekau <alex@apnic.net>
2019-02-25 19:40:22 +1000

    test

--- test.sh
+++ test.sh
@@ -1,15 +1,14 @@
 #!/usr/bin/env bash
 
-vbase=$( date +v%Y%m%d )
-echo release
-release=1
-count=1
-while [ $count -ne "0" ]; do
-  count=`git tag | grep $vbase-$release | wc -l`
-  if [ $count -ne "0" ]; then
-    release=$(( $release + 1 ))
-    echo new release $release
-  fi
-done
-
-echo $vbase-$release
+    vbase=$( date +v%Y%m%d )
+    echo release
+    release=1
+    count=1
+    while [ $count -ne "0" ]; do
+      count=`git tag | grep $vbase-$release | wc -l`
+      if [ $count -ne "0" ]; then
+        release=$(( $release + 1 ))
+        echo new release $release
+      fi
+    done
+    echo Tag $version
