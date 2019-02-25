c2e0c59551aa9859c3d897731f1c1d6972e17615
Alex Avekau <alex@apnic.net>
2019-02-25 20:00:40 +1000

    test

--- test.sh
+++ test.sh
@@ -5,7 +5,7 @@
     release=1
     count=1
     while true; do
-      count=`git tag | grep $vbase-$release | wc -l`
+      count=`git tag | grep $vbase-$release | wc -l` || "0"
       if [ $count -ne "0" ]
        then release=$(( $release + 1 ))
       else
