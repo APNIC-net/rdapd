25d7d8ea782d660e379b9aadef2a90f3f32c4650
Alex Avekau <alex@apnic.net>
2019-03-29 08:22:14 +1000

    test

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -5,6 +5,6 @@ echo "Launching with arguments: $@"
 JVM_INIT_MEM="${JVM_INIT_MEM:-4G}"
 JVM_MAX_MEM="${JVM_MAX_MEM:-8G}"
 
-exec java -Xms${JVM_INIT_MEM} -Xmx${JVM_MAX_MEM} \
-	-jar /app/@project.artifactId@-@project.version@.@project.packaging@ \
-	--spring.config.location=/app/config/application.yml
+set -e
+echo "Starting application"
+exec java -jar @project.artifactId@-@project.version@.@project.packaging@ "$@"
