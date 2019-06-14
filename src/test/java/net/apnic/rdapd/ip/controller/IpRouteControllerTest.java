cca17e7e84e64387545877cf75b70c45f6cf5f5e
Rafael Cintra <rafael@apnic.net>
2019-06-14 14:43:04 +1000

    [SCRUM-4267] Fixed issue with IPv4 prefix splitting.
    The issue was being triggered when the prefix's highest octet was larger
    than 127.

--- src/test/java/net/apnic/rdapd/ip/controller/IpRouteControllerTest.java
+++ src/test/java/net/apnic/rdapd/ip/controller/IpRouteControllerTest.java
@@ -167,6 +167,40 @@ public class IpRouteControllerTest
     }
 
     @Test
+    public void testCidr0HighIpv4WithMultipleCidrEntries() throws Exception {
+        given(ipService.find(any())).willReturn(
+                Optional.of(new IpNetwork(
+                        new ObjectKey(ObjectClass.IP_NETWORK, "202.75.0.1 - 202.75.0.255"),
+                        Parsing.parseInterval("202.75.0.1 - 202.75.0.255"))));
+
+        MvcResult mvcResult = mvc.perform(get("/ip/202.75.0.1"))
+                .andExpect(status().isOk())
+                .andExpect(isRDAP())
+                .andReturn();
+
+        String content = mvcResult.getResponse().getContentAsString();
+        ObjectMapper mapper = new ObjectMapper();
+        JsonNode json = mapper.readTree(content);
+
+        assertThat(json.get("cidr0_cidrs"), notNullValue());
+        assertThat(json.get("cidr0_cidrs").size(), is(8));
+
+        List<Tuple<String, Integer>> jsonPrefixes =
+                StreamSupport.stream(json.get("cidr0_cidrs").spliterator(), false)
+                        .map(node -> Tuple.of(node.get("v4prefix").textValue(), node.get("length").intValue()))
+                        .collect(Collectors.toList());
+
+        assertThat(jsonPrefixes, hasItem(Tuple.of("202.75.0.1", 32)));
+        assertThat(jsonPrefixes, hasItem(Tuple.of("202.75.0.2", 31)));
+        assertThat(jsonPrefixes, hasItem(Tuple.of("202.75.0.4", 30)));
+        assertThat(jsonPrefixes, hasItem(Tuple.of("202.75.0.8", 29)));
+        assertThat(jsonPrefixes, hasItem(Tuple.of("202.75.0.16", 28)));
+        assertThat(jsonPrefixes, hasItem(Tuple.of("202.75.0.32", 27)));
+        assertThat(jsonPrefixes, hasItem(Tuple.of("202.75.0.64", 26)));
+        assertThat(jsonPrefixes, hasItem(Tuple.of("202.75.0.128", 25)));
+    }
+
+    @Test
     public void testCidr0Ipv6() throws Exception {
         given(ipService.find(any())).willReturn(
                 Optional.of(new IpNetwork(
@@ -185,6 +219,28 @@ public class IpRouteControllerTest
         assertThat(json.get("cidr0_cidrs"), notNullValue());
         assertThat(json.get("cidr0_cidrs").size(), is(1));
         assertThat(json.get("cidr0_cidrs").get(0).get("v6prefix").textValue(), is("2001:db8::"));
+        assertThat(json.get("cidr0_cidrs").get(0).get("length").intValue(), is(32));
+    }
+
+    @Test
+    public void testCidr0HighIpv6() throws Exception {
+        given(ipService.find(any())).willReturn(
+                Optional.of(new IpNetwork(
+                        new ObjectKey(ObjectClass.IP_NETWORK, "ffee:db8:: - 2001:db8:0:ffff:ffff:ffff:ffff:ffff"),
+                        Parsing.parseInterval("ffee:db8::/32"))));
+
+        MvcResult mvcResult = mvc.perform(get("/ip/ffee:db8::"))
+                .andExpect(status().isOk())
+                .andExpect(isRDAP())
+                .andReturn();
+
+        String content = mvcResult.getResponse().getContentAsString();
+        ObjectMapper mapper = new ObjectMapper();
+        JsonNode json = mapper.readTree(content);
+
+        assertThat(json.get("cidr0_cidrs"), notNullValue());
+        assertThat(json.get("cidr0_cidrs").size(), is(1));
+        assertThat(json.get("cidr0_cidrs").get(0).get("v6prefix").textValue(), is("ffee:db8::"));
         assertThat(json.get("cidr0_cidrs").get(0).get("length").intValue(), is(32));
     }
 }
