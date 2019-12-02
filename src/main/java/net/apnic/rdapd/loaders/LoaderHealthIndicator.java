72c7bdaf4abbf1d5b12f2318ac29b80b41d1c31a
Rafael Cintra <rafael@apnic.net>
2019-12-02 17:23:49 +1000

    [RST-88] Added support for prometheus endpoint.

--- src/main/java/net/apnic/rdapd/loaders/LoaderHealthIndicator.java
+++ src/main/java/net/apnic/rdapd/loaders/LoaderHealthIndicator.java
@@ -1,8 +1,10 @@
 package net.apnic.rdapd.loaders;
 
+import io.prometheus.client.Gauge;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.actuate.health.Health;
 import org.springframework.boot.actuate.health.HealthIndicator;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
 /**
@@ -11,11 +13,18 @@ import org.springframework.stereotype.Component;
 @Component
 public class LoaderHealthIndicator implements HealthIndicator {
 
+    private static final Gauge PROMETHEUS_LOADER_STATUS =
+            Gauge.build()
+                    .name("rdapd_loader_status")
+                    .help("RDAP loader status (" + getLoaderStatusesString() + ")")
+                    .register();
+
     private final LoaderStatusProvider loaderStatusProvider;
 
     @Autowired
     public LoaderHealthIndicator(LoaderStatusProvider loaderStatusProvider) {
         this.loaderStatusProvider = loaderStatusProvider;
+        updatePrometheusStatus();
     }
 
     @Override
@@ -29,5 +38,21 @@ public class LoaderHealthIndicator implements HealthIndicator {
                                                 ? loaderStatus.getLastSuccessfulDateTime().get() : "never") +
                 ", status=" + loaderStatus.getStatus() +
                 '}';
+    }
+
+    @Scheduled(fixedRate = 60 * 1000 /* 1 min */)
+    public void updatePrometheusStatus() {
+        PROMETHEUS_LOADER_STATUS.set(loaderStatusProvider.getLoaderStatus().getStatus().ordinal());
+    }
+
+    private static String getLoaderStatusesString() {
+        LoaderStatus.Status[] statuses = LoaderStatus.Status.values();
+        String[] strings = new String[statuses.length];
+
+        for (int counter = 0; counter < statuses.length; counter++) {
+            strings[counter] = counter + ": " + statuses[counter];
+        }
+
+        return String.join(", ", strings);
     }
 }
